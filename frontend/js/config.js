2fa0ebb6229e84851d04d3b0a5dd90362cd59bdb
andra-sun <camileguimaraes463@gmail.com>
2026-01-29 10:19:47 -0300

    url do backend atualizada

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,8 +2,9 @@
  * Configuração da aplicação
  */
 
-export const BACKEND_URL = "http://localhost:8000";
+// export const BACKEND_URL = "http://localhost:8000";
 // export const BACKEND_URL = "https://email-w3m9.onrender.com";
+export const BACKEND_URL = "email-production-55e8.up.railway.app";
 
 export default {
     BACKEND_URL,
